dab1359593f5162fdbb176c928e053a21b04139a
nguyen zan <114898603+zannngyn@users.noreply.github.com>
2024-04-11 10:08:33 +0000

    test API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,22 +11,22 @@ const $$ = document.querySelectorAll.bind(document);
  * </script>
  */
 function load(selector, path) {
-    const cached = localStorage.getItem(path);
-    if (cached) {
-        $(selector).innerHTML = cached;
-    }
+  const cached = localStorage.getItem(path);
+  if (cached) {
+    $(selector).innerHTML = cached;
+  }
 
-    fetch(path)
-        .then((res) => res.text())
-        .then((html) => {
-            if (html !== cached) {
-                $(selector).innerHTML = html;
-                localStorage.setItem(path, html);
-            }
-        })
-        .finally(() => {
-            window.dispatchEvent(new Event("template-loaded"));
-        });
+  fetch(path)
+    .then((res) => res.text())
+    .then((html) => {
+      if (html !== cached) {
+        $(selector).innerHTML = html;
+        localStorage.setItem(path, html);
+      }
+    })
+    .finally(() => {
+      window.dispatchEvent(new Event("template-loaded"));
+    });
 }
 
 /**
@@ -34,21 +34,21 @@ function load(selector, path) {
  * có bị ẩn bởi display: none không
  */
 function isHidden(element) {
-    if (!element) return true;
+  if (!element) return true;
 
-    if (window.getComputedStyle(element).display === "none") {
-        return true;
+  if (window.getComputedStyle(element).display === "none") {
+    return true;
+  }
+
+  let parent = element.parentElement;
+  while (parent) {
+    if (window.getComputedStyle(parent).display === "none") {
+      return true;
     }
+    parent = parent.parentElement;
+  }
 
-    let parent = element.parentElement;
-    while (parent) {
-        if (window.getComputedStyle(parent).display === "none") {
-            return true;
-        }
-        parent = parent.parentElement;
-    }
-
-    return false;
+  return false;
 }
 
 /**
@@ -56,13 +56,13 @@ function isHidden(element) {
  * sau một khoảng thời gian mới được thực thi
  */
 function debounce(func, timeout = 300) {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            func.apply(this, args);
-        }, timeout);
-    };
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
 
 /**
@@ -73,14 +73,14 @@ function debounce(func, timeout = 300) {
  * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
  */
 const calArrowPos = debounce(() => {
-    if (isHidden($(".js-dropdown-list"))) return;
+  if (isHidden($(".js-dropdown-list"))) return;
 
-    const items = $$(".js-dropdown-list > li");
+  const items = $$(".js-dropdown-list > li");
 
-    items.forEach((item) => {
-        const arrowPos = item.offsetLeft + item.offsetWidth / 2;
-        item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
-    });
+  items.forEach((item) => {
+    const arrowPos = item.offsetLeft + item.offsetWidth / 2;
+    item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
+  });
 });
 
 // Tính toán lại vị trí arrow khi resize trình duyệt
@@ -100,43 +100,43 @@ window.addEventListener("template-loaded", calArrowPos);
 window.addEventListener("template-loaded", handleActiveMenu);
 
 function handleActiveMenu() {
-    const dropdowns = $$(".js-dropdown");
-    const menus = $$(".js-menu-list");
-    const activeClass = "menu-column__item--active";
+  const dropdowns = $$(".js-dropdown");
+  const menus = $$(".js-menu-list");
+  const activeClass = "menu-column__item--active";
 
-    const removeActive = (menu) => {
-        menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
-    };
+  const removeActive = (menu) => {
+    menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
+  };
 
-    const init = () => {
-        menus.forEach((menu) => {
-            const items = menu.children;
-            if (!items.length) return;
+  const init = () => {
+    menus.forEach((menu) => {
+      const items = menu.children;
+      if (!items.length) return;
 
-            removeActive(menu);
-            if (window.innerWidth > 991) items[0].classList.add(activeClass);
+      removeActive(menu);
+      if (window.innerWidth > 991) items[0].classList.add(activeClass);
 
-            Array.from(items).forEach((item) => {
-                item.onmouseenter = () => {
-                    if (window.innerWidth <= 991) return;
-                    removeActive(menu);
-                    item.classList.add(activeClass);
-                };
-                item.onclick = () => {
-                    if (window.innerWidth > 991) return;
-                    removeActive(menu);
-                    item.classList.add(activeClass);
-                    item.scrollIntoView();
-                };
-            });
-        });
-    };
-
-    init();
-
-    dropdowns.forEach((dropdown) => {
-        dropdown.onmouseleave = () => init();
+      Array.from(items).forEach((item) => {
+        item.onmouseenter = () => {
+          if (window.innerWidth <= 991) return;
+          removeActive(menu);
+          item.classList.add(activeClass);
+        };
+        item.onclick = () => {
+          if (window.innerWidth > 991) return;
+          removeActive(menu);
+          item.classList.add(activeClass);
+          item.scrollIntoView();
+        };
+      });
     });
+  };
+
+  init();
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.onmouseleave = () => init();
+  });
 }
 
 /**
@@ -149,82 +149,100 @@ function handleActiveMenu() {
 window.addEventListener("template-loaded", initJsToggle);
 
 function initJsToggle() {
-    $$(".js-toggle").forEach((button) => {
-        const target = button.getAttribute("toggle-target");
-        if (!target) {
-            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+  $$(".js-toggle").forEach((button) => {
+    const target = button.getAttribute("toggle-target");
+    if (!target) {
+      document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+    }
+    button.onclick = (e) => {
+      e.preventDefault();
+
+      if (!$(target)) {
+        return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+      }
+      const isHidden = $(target).classList.contains("hide");
+
+      requestAnimationFrame(() => {
+        $(target).classList.toggle("hide", !isHidden);
+        $(target).classList.toggle("show", isHidden);
+      });
+    };
+    document.onclick = function (e) {
+      if (!e.target.closest(target)) {
+        const isHidden = $(target).classList.contains("hide");
+        if (!isHidden) {
+          button.click();
         }
-        button.onclick = (e) => {
-            e.preventDefault();
-
-            if (!$(target)) {
-                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
-            }
-            const isHidden = $(target).classList.contains("hide");
-
-            requestAnimationFrame(() => {
-                $(target).classList.toggle("hide", !isHidden);
-                $(target).classList.toggle("show", isHidden);
-            });
-        };
-        document.onclick = function (e) {
-            if (!e.target.closest(target)) {
-                const isHidden = $(target).classList.contains("hide");
-                if (!isHidden) {
-                    button.click();
-                }
-            }
-        };
-    });
+      }
+    };
+  });
 }
 
 window.addEventListener("template-loaded", () => {
-    const links = $$(".js-dropdown-list > li > a");
+  const links = $$(".js-dropdown-list > li > a");
 
-    links.forEach((link) => {
-        link.onclick = () => {
-            if (window.innerWidth > 991) return;
-            const item = link.closest("li");
-            item.classList.toggle("navbar__item--active");
-        };
-    });
+  links.forEach((link) => {
+    link.onclick = () => {
+      if (window.innerWidth > 991) return;
+      const item = link.closest("li");
+      item.classList.toggle("navbar__item--active");
+    };
+  });
 });
 
 window.addEventListener("template-loaded", () => {
-    const tabsSelector = "prod-tab__item";
-    const contentsSelector = "prod-tab__content";
+  const tabsSelector = "prod-tab__item";
+  const contentsSelector = "prod-tab__content";
 
-    const tabActive = `${tabsSelector}--current`;
-    const contentActive = `${contentsSelector}--current`;
+  const tabActive = `${tabsSelector}--current`;
+  const contentActive = `${contentsSelector}--current`;
 
-    const tabContainers = $$(".js-tabs");
-    tabContainers.forEach((tabContainer) => {
-        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
-        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
-        tabs.forEach((tab, index) => {
-            tab.onclick = () => {
-                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
-                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
-                tab.classList.add(tabActive);
-                contents[index].classList.add(contentActive);
-            };
-        });
+  const tabContainers = $$(".js-tabs");
+  tabContainers.forEach((tabContainer) => {
+    const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+    const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+    tabs.forEach((tab, index) => {
+      tab.onclick = () => {
+        tabContainer
+          .querySelector(`.${tabActive}`)
+          ?.classList.remove(tabActive);
+        tabContainer
+          .querySelector(`.${contentActive}`)
+          ?.classList.remove(contentActive);
+        tab.classList.add(tabActive);
+        contents[index].classList.add(contentActive);
+      };
     });
+  });
 });
 
 window.addEventListener("template-loaded", () => {
-    const switchBtn = document.querySelector("#switch-theme-btn");
-    if (switchBtn) {
-        switchBtn.onclick = function () {
-            const isDark = localStorage.dark === "true";
-            document.querySelector("html").classList.toggle("dark", !isDark);
-            localStorage.setItem("dark", !isDark);
-            switchBtn.querySelector("span").textContent = isDark ? "Dark mode" : "Light mode";
-        };
-        const isDark = localStorage.dark === "true";
-        switchBtn.querySelector("span").textContent = isDark ? "Light mode" : "Dark mode";
-    }
+  const switchBtn = document.querySelector("#switch-theme-btn");
+  if (switchBtn) {
+    switchBtn.onclick = function () {
+      const isDark = localStorage.dark === "true";
+      document.querySelector("html").classList.toggle("dark", !isDark);
+      localStorage.setItem("dark", !isDark);
+      switchBtn.querySelector("span").textContent = isDark
+        ? "Dark mode"
+        : "Light mode";
+    };
+    const isDark = localStorage.dark === "true";
+    switchBtn.querySelector("span").textContent = isDark
+      ? "Light mode"
+      : "Dark mode";
+  }
 });
 
 const isDark = localStorage.dark === "true";
 document.querySelector("html").classList.toggle("dark", isDark);
+
+// API Tỉnh thành dùng cho địa chỉ khách hàng
+fetch("https://vn-public-apis.fpo.vn/wards/getAll?limit=-1")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Loi call API", error);
+  });
