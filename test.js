fa6ecc4f1a95c03896b45869a577174a4c809c06
Riacuaa <haohopnguyen@gmail.com>
2024-04-26 01:30:39 +0700

    Chỉnh sửa và thêm trang chi tiết khuyến mãi

--- test.js
+++ test.js
@@ -615,6 +615,11 @@ app.get("/payment", async (req, res) => {
     price: price,
   });
 });
+app.get("/hotdeal_child_samsung", async (req, res) => {
+  res.render("hotdeal_child_samsung.ejs", {
+    check: false,
+  });
+});
 app.get("/hotdeal_child_oppo", async (req, res) => {
   res.render("hotdeal_child_oppo.ejs", {
     check: false,
